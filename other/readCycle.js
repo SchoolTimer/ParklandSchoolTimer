0fb1717f2f121e9508edeeb741335a2707329d7d
Glitch (schooltimer) <none>
2021-11-17 17:54:01 +0000

    👴💅 Checkpoint ./other/readCycle.js:2055672/4311

--- other/readCycle.js
+++ other/readCycle.js
@@ -1,17 +1,6 @@
-function getDate() //gets the date
-{
-  let  today = new Date();
-  let  dd = String(today.getDate()).padStart(2, '0');
-  let  mm = String(today.getMonth() + 1).padStart(2, '0');
-  let  yyyy     = today.getFullYear();
-  return mm + '-' + dd + '-' + yyyy;
-}
-
 function readJsonData() {
-  date = getDate(); //gets the date from the function from above
-  console.log(date)
   
-  fetch("https://613aad1a110e000017a45364.mockapi.io/dayCycleBeta/" + date, { cache: "reload" })
+  fetch("https://613aad1a110e000017a45364.mockapi.io/dayCycleBeta/1", { cache: "reload" }) 
 
     .then(response => response.json())
     .then(data => {
@@ -20,6 +9,7 @@ function readJsonData() {
       document.getElementById("cycleDayTomorrow").innerHTML = data.tomorrow;
       document.getElementById("cycleDayNextDay").innerHTML = data.nextDay;
     });
+
   setTimeout(readJsonData, 60*12*60*1000);
 }
 readJsonData();
