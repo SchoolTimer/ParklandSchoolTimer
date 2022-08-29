68a0e67aac79ad64dc216c119623278a9a27d427
Glitch (schooltimer) <none>
2022-08-29 01:43:51 +0000

    🌑🚴 Checkpoint ./other/readCycle.js:36557637/172

--- other/readCycle.js
+++ other/readCycle.js
@@ -54,10 +54,10 @@ function readJsonData() {
 
     .then(response => response.json())
     .then(data => {
-      document.getElementById("cycleDayOutput").innerHTML = data.today; 
-      document.getElementById("cycleDayToday").innerHTML = data.today;
-      document.getElementById("cycleDayTomorrow").innerHTML = data.tomorrow;
-      document.getElementById("cycleDayNextDay").innerHTML = data.nextDay;
+      document.getElementById("cycleDayOutput").innerHTML = 'B'; 
+      document.getElementById("cycleDayToday").innerHTML = 'B';
+      document.getElementById("cycleDayTomorrow").innerHTML = 'B'
+      document.getElementById("cycleDayNextDay").innerHTML = 'A';
       document.getElementById("currentMonth").innerHTML = data.month;
       // document.getElementById("currentSchedule").innerHTML = data.currentSchedule;
     });
