c0d1b9061f19a6744fefa314a89245538b32c4b3
Glitch (schooltimer) <none>
2022-08-30 02:29:31 +0000

    fixed scaling issue

--- other/stats.js
+++ other/stats.js
@@ -33,7 +33,7 @@ for (var i = 0; i < deadlines.length; i++) {
   label.style.marginRight = '10px';
   label.style.margin = '0px;';
   label.style.marginLeft = '0vw';
-  label.style.fontSize = '50px';
+  label.style.fontSize = '25px';
   label.style.marginBottom = '0';
 
   var output = document.createElement('h4');
