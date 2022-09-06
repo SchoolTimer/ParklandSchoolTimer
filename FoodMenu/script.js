bcce2fe0a9fcde577e168d53db861654cc9bd049
Krushil Amrutiya <krushilamrutiya@gmail.com>
2022-09-06 13:21:31 -0400

    Add files via upload

---
+++ FoodMenu/script.js
@@ -0,0 +1,53 @@
+async function readJsonDataFoodMenu() {
+  var breakfastDiv = document.getElementById("breakfastMenu");
+  breakfastDiv.innerHTML = "Loading :/";
+
+  var lunchDiv = document.getElementById("lunchMenu");
+  lunchDiv.innerHTML = "Loading :/";
+
+  await fetch("https://6266b23763e0f3825685c4a6.mockapi.io/LunchBreakfastAPI/1")
+    .then((response) => response.json())
+    .then((data) => {
+      // Breakfast items
+      var breakfastObjSize = data.data.data.site0.items.length;
+      var breakfastDiv = document.getElementById("breakfastMenu");
+      breakfastDiv.innerHTML = "";
+      for (let i = 0; i < breakfastObjSize; i++) {
+        if (i == 0) {
+          let breakfast = data.data.data.site0.items[i].product.name;
+          breakfast = breakfast.charAt(0).toUpperCase() + breakfast.slice(1);
+          breakfastDiv.innerHTML += breakfast;
+        } else {
+          let breakfast = data.data.data.site0.items[i].product.name;
+          breakfastUpperCase =
+            breakfast.charAt(0).toUpperCase() + breakfast.slice(1);
+          breakfastFormated = ", " + breakfastUpperCase;
+          breakfastDiv.innerHTML += breakfastFormated;
+        }
+      }
+
+      // var LunchObjSize = data.data.data.site1.items.length;
+      var LunchObjSize = data.data.data.site1.items.length;
+      var lunchDiv = document.getElementById("lunchMenu");
+      lunchDiv.innerHTML = "";
+      for (let i = 0; i < LunchObjSize; i++) {
+        if (i == 0) {
+          //   let lunch = data.data.data.site1.items[i].product.name;
+          //   lunchDiv.innerHTML += lunch;
+          let lunch = data.data.data.site1.items[i].product.name;
+          lunch = lunch.charAt(0).toUpperCase() + lunch.slice(1);
+          lunchDiv.innerHTML += lunch;
+        } else {
+          //   let lunch = ", " + data.data.data.site1.items[i].product.name;
+          //   lunchDiv.innerHTML += lunch;
+          let lunch = data.data.data.site1.items[i].product.name;
+          lunchUpperCase = lunch.charAt(0).toUpperCase() + lunch.slice(1);
+          lunchFormated = ", " + lunchUpperCase;
+          lunchDiv.innerHTML += lunchFormated;
+        }
+      }
+    });
+
+  setTimeout(readJsonDataFoodMenu, 60 * 12 * 60 * 1000);
+}
+readJsonDataFoodMenu();
